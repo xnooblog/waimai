c98cd72bcc5e657360dcfef716e3423eeda6d31c
xnooblog <2445322902@qq.com>
2021-08-17 20:18:54 +0800

    完成MSite顶部栏

--- src/components/HeaderTop/HeaderTop.vue
+++ src/components/HeaderTop/HeaderTop.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="header">
     <slot name="left"></slot>
-    <span class="head_title">
-      <span class="head_title_text"></span>
+    <span class="header_title">
+      <span class="header_title_text ellipsis">{{title}}</span>
     </span>
     <slot name="right"></slot>
   </header>
@@ -18,5 +18,41 @@
 </script>
 
 <style lang="stylus" rel="sheetstyle/stylus">
-
+  .header
+    position fixed
+    left 0
+    top 0
+    width 100%
+    height 45px
+    background-color #02A774
+    z-index 1000
+    .header_search
+      position absolute
+      left 15px
+      top 50%
+      transform translateY(-50%)
+      width 10%
+      height 50%
+      .icon-sousuo
+        font-size 25px
+        color #fff
+    .header_title
+      position absolute
+      left 50%
+      top 50%
+      width 50%
+      transform translate(-50%, -50%)
+      color #fff
+      text-align center
+      .header_title_text
+        display block
+        font-size 20px
+        color #fff
+    .header_login
+      position absolute
+      right 15px
+      top 50%
+      transform translateY(-50%)
+      .header_login_text
+        color #fff
 </style>
--- src/pages/MSite/MSite.vue
+++ src/pages/MSite/MSite.vue
@@ -1,8 +1,15 @@
 <template>
   <section class="msite">
     <!--首页头部-->
-    <HeaderTop :title="'aaa'">
-
+    <HeaderTop :title="address.name">
+      <router-link class="header_search" slot="left" to="/search">
+        <i class="iconfont icon-sousuo"></i>
+      </router-link>
+      <router-link class="header_login" slot="right" to="/login">
+        <span class="header_login_text">
+          登录|注册
+        </span>
+      </router-link>
     </HeaderTop>
   </section>
 </template>
@@ -14,7 +21,10 @@
     name: 'MSite',
     data() {
       return {
-        baseImageUrl: 'https://fuss10.elemecdn.com'
+        baseImageUrl: 'https://fuss10.elemecdn.com',
+        address: {
+          name: '北京市昌平区337省道'
+        }
       }
     },
     computed: {
